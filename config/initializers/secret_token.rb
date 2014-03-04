6b1a6b6f16e9da5dc3edbea3091fb36a5618b421
darin.thompson <darint07@gmail>
2014-03-03 21:04:02 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloctodo::Application.config.secret_token = 'f6b93a5195d9ecf6057cde890c01a3d581e293c08c06acfc18a7c47c4aadda105167672121f9cb81f711f99467bd7c3253cf00a8909dd1f0581aa832b48be574'
